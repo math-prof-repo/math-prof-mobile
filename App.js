f35d5190917ccb79b5c59bd6dbdfa6e6142f5dcd
Mustafa Uzunkaya <mustafauzunkaya07@gmail.com>
2018-01-30 00:34:59 +0300

    loader

--- App.js
+++ App.js
@@ -6,6 +6,10 @@ import {
   StyleSheet
 } from 'react-native';
 
+import { Bubbles, DoubleBounce, Bars, Pulse } from 'react-native-loader';
+
+var axios = require('axios');
+
 import {
   StackNavigator,
 } from 'react-navigation';
@@ -15,32 +19,53 @@ import HomeScreen from './pages/Home';
 import Login from './pages/Login';
 
 class MathProf extends React.Component {
-  
-  componentDidMount(){
+  constructor(props) {
+    super(props);
+    this.state = { name: '' };
+  }
+  componentDidMount() {
     /*
     user control
       this.props.navigation.navigate('Result');
       or
       
     */
-    this.props.navigation.navigate('Home');
+    this.methodGet('http://yeslimit.somee.com/api/user');
+
+    //this.props.navigation.navigate('Home');
 
   }
+
+  methodGet(param) {
+    axios.get(param + '?userId=' + Expo.Constants.deviceId)
+      .then((response) => {
+        if (response.data.UserId == undefined || response.data.UserId == null || response.data.UserId == null) {
+          this.props.navigation.navigate('Login');
+        } else {
+          this.props.navigation.navigate('Home');
+        }
+      }).catch((error) => {
+        console.log(error);
+      });
+
+  };
 
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity>
-          <Text>
-            Yükleniyor
-          </Text>
-        </TouchableOpacity>
+        <Bubbles size={10} color="#FFF" />
+        <Bars size={10} color="#FDAAFF" />
+        <Pulse size={10} color="#52AB42" />
+        <DoubleBounce size={10} color="#1CAFF6" />
       </View>
     )
   }
 }
 
 export default StackNavigator({
+  MathProf: {
+    screen: MathProf
+  },
   Home: {
     screen: HomeScreen
   },
@@ -50,7 +75,58 @@ export default StackNavigator({
   Result: {
     screen: Result
   },
-  Login:{
-    screen:Login
+  Login: {
+    screen: Login
   }
 });
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  input: {
+    flex: 1,
+    paddingTop: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
+    paddingLeft: 0,
+    backgroundColor: '#fff',
+    color: '#424242',
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+  }
+});
+
+
+async function showFirstContactAsync() {
+  // Ask for permission to query contacts.
+  console.log(Expo.Constants);
+  /*const permission = await Expo.Permissions.askAsync(Expo.Permissions.CONTACTS);
+  if (permission.status !== 'granted') {
+    // Permission was denied...
+    return;
+  }
+  const contacts = await Expo.Contacts.getContactsAsync({
+    fields: [
+      Expo.Contacts.PHONE_NUMBERS,
+      Expo.Contacts.EMAILS,
+    ],
+    pageSize: 10,
+    pageOffset: 0,
+  });
+  if (contacts.total > 0) {
+    Alert.alert(
+      'Your first contact is...',
+      `Name: ${contacts.data[0].name}\n` +
+      `Phone numbers: ${JSON.stringify(contacts.data[0].phoneNumbers)}\n` +
+      `Emails: ${JSON.stringify(contacts.data[0].emails)}`
+    );
+  }*/
+}
--- pages/Login.js
+++ pages/Login.js
@@ -1,9 +1,84 @@
 import React from 'react';
+import {
+    Text,
+    View,
+    TouchableOpacity,
+    StyleSheet,
+    TextInput,
+    Button
+} from 'react-native';
 
+var axios = require('axios');
 export default class Login extends React.Component {
-    render(){
-        return({
-            
-        });
+
+    constructor(props) {
+        super(props);
+        this.state = { name: '' };
+    }
+
+    methodPost(param) {
+        var self=this;
+        let userData = { UserId: Expo.Constants.deviceId, UserName: this.state.name };
+        axios.interceptors.request.use(request => {
+            console.log('Starting Request', request)
+            return request
+        })
+
+        axios.post(param,
+            userData
+        )
+            .then(function (response) {
+                console.log(response);
+                self.props.navigation.navigate('Home');
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    
+
+    render() {
+        return (
+            <View>
+                <Text>
+                    User Name:
+                </Text>
+                <TextInput
+                    placeholder="User Nickname"
+                    onChangeText={(name) => { this.setState({ name }) }}
+                    underlineColorAndroid="transparent"
+                />
+                <Button
+                    onPress={() => {
+                        this.methodPost('http://yeslimit.somee.com/api/user');
+                    }}
+                    title="Kaydol"
+                    color="#841584"
+                />
+            </View>
+        );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+    input: {
+        flex: 1,
+        paddingTop: 10,
+        paddingRight: 10,
+        paddingBottom: 10,
+        paddingLeft: 0,
+        backgroundColor: '#fff',
+        color: '#424242',
+        height: 80,
+        borderColor: 'black',
+        borderWidth: 1,
+    }
+});
